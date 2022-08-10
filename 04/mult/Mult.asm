f15c5a2e5b1f89c0005f9f9c3ad49447b1f6fc5c
kiyodori <kiyodori@yahoo.co.jp>
2022-08-10 11:10:13 +0900

    add projects

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,32 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
+// Translates .asm to .hack
+// tools/Assembler.sh projects/04/mult/Mult.asm
+
 // Put your code here.
+
+// 掛け算をループでやる
+
+    @R2
+    M=0 // M[R2] = 0
+    @i
+    M=1 // M[i] = 1
+(LOOP)
+    @i
+    D=M // D = i
+    @R1
+    D=D-M // D = i - R1
+    @END
+    D;JGT // if i - R2 > 0 then goto @END
+    @R0
+    D=M // D = R0
+    @R2
+    M=D+M // D = R0 + M[R2]
+    @i
+    M=M+1 // M[i]++
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
